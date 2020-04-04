b43e1f2ec8094c282ca84f1e4655e49c385ea01a
nicolasklz <nicolaskleinhentz@gmail.com>
2020-04-04 16:25:14 +0200

    +user inputs & apprentissage à partir d'une taille définie de la base d'apprentissage

--- src/FiltreAntiSpam.java
+++ src/FiltreAntiSpam.java
@@ -1,9 +1,21 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class FiltreAntiSpam {
 
+    private static int nbSPAMapp; //nb de spam pour l'apprentissage
+    private static int nbHAMapp; //nb de ham pour l'apprentissage
+    private static ArrayList<String> dictionnaire;
+    private static int eps = 1;
+    private static double[] b_spam;
+    private static double[] b_ham;
+    private static double P_Y_spam;
+    private static double P_Y_ham;
+
+    private static final String DOSSIER_SPAM = "res/baseapp/spam/";
+    private static final String DOSSIER_HAM = "res/baseapp/ham/";
     private static final String DICTIONNAIRE = "res/dictionnaire1000en.txt";
 
     public static void main (String[] args) {
@@ -14,16 +26,30 @@ public class FiltreAntiSpam {
 
         //récupération des arguments
         String baseTests = args[0];
-        int nbSPAM = Integer.parseInt(args[1]), nbHAM = Integer.parseInt(args[2]);
-        System.out.println("Dossier de tests : " + baseTests + " sur " + nbSPAM + " SPAM et " + nbHAM + " HAM"); //DEBUG
+        int nbSPAMtest = Integer.parseInt(args[1]), nbHAMtest = Integer.parseInt(args[2]);
+        System.out.println("Dossier de tests : " + baseTests + " sur " + nbSPAMtest + " SPAM et " + nbHAMtest + " HAM"); //DEBUG
 
-        ArrayList<String> dictionnaire = charger_dictionnaire(DICTIONNAIRE);
-        System.out.println("Le dico contient " + dictionnaire.size() + " mots"); //DEBUG
+        Scanner scan = new Scanner(System.in);
 
-        HashMap<Integer, Integer> vecteurMots = lire_message(dictionnaire, "res/basetest/spam/0.txt");
-        int res = 0;
-        for (Integer i : vecteurMots.values()) if(i == 1) res+=1;
-        System.out.println("Vecteur de mots de taille : " + vecteurMots.size() + " avec " + res + " mots du dico présents");
+        System.out.print("Combien de SPAM dans la base d'apprentissage ? ");
+        nbSPAMapp = scan.nextInt();
+        System.out.print("Combien de HAM dans la base d'apprentissage ? ");
+        nbHAMapp = scan.nextInt();
+
+        System.out.println("\nChargement du dictionnaire...");
+        dictionnaire = charger_dictionnaire(DICTIONNAIRE);
+
+        System.out.println("\nApprentissage...");
+        apprentissage();
+
+        P_Y_spam = (double)nbSPAMapp / (double)(nbSPAMapp + nbHAMapp);
+        P_Y_ham = (float)nbHAMapp / (float)(nbSPAMapp + nbHAMapp);
+
+        System.out.println("Probas à priori :"); //DEBUG
+        System.out.println("P(Y = SPAM) = " + P_Y_spam); //DEBUG
+        System.out.println("P(Y = HAM) = " + P_Y_ham); //DEBUG
+
+        //need calcul des probas à priori, qu'est ce que P(X = x) ?
     }
 
     private static ArrayList<String> charger_dictionnaire (String nomFichier) {
@@ -74,6 +100,8 @@ public class FiltreAntiSpam {
             while ((ligne = br.readLine()) != null) {
                 mots = ligne.split(" ");
                 for (String mot : mots) {
+                    mot = mot.replaceAll("[^a-zA-Z]", ""); //suppression des caractères autre que les lettres
+                    mot = mot.toUpperCase();
                     int index = dictionnaire.indexOf(mot);
                     if (index != -1 && vecteurMots.get(index) == 0) {
                         vecteurMots.put(index, 1);
@@ -87,6 +115,49 @@ public class FiltreAntiSpam {
         }
 
         return null; //il y a eu une erreur
+    }
+
+    private static void apprentissage () {
+        apprentissageSPAM();
+        apprentissageHAM();
+    }
+
+    private static void apprentissageSPAM () {
+        //parcours des m_spam et m_ham
+        // b_spam^j = nb spam contenant mot j / nb spam
+        b_spam = new double[dictionnaire.size()];
+
+        for (int j = 0; j < nbSPAMapp; j++) {
+            HashMap<Integer, Integer> occurences = lire_message(dictionnaire, DOSSIER_SPAM + j + ".txt");
+            for (int i = 0; i < dictionnaire.size(); i++) {
+                if (occurences.get(i) == 1) {
+                    b_spam[i]++;
+                }
+            }
+        }
+
+        for (int i = 0; i < dictionnaire.size(); i++) {
+            b_spam[i] = (b_spam[i] + eps) / (nbSPAMapp + (2 * eps));
+        }
+    }
+
+    private static void apprentissageHAM () {
+        //parcours des m_spam et m_ham
+        // b_ham^j = nb ham contenant mot j / nb ham
+        b_ham = new double[dictionnaire.size()];
+
+        for (int j = 0; j < nbHAMapp; j++) {
+            HashMap<Integer, Integer> occurences = lire_message(dictionnaire, DOSSIER_HAM + j + ".txt");
+            for (int i = 0; i < dictionnaire.size(); i++) {
+                if (occurences.get(i) == 1) {
+                    b_ham[i]++;
+                }
+            }
+        }
+
+        for (int i = 0; i < dictionnaire.size(); i++) {
+            b_ham[i] = (b_ham[i] + eps) / (nbHAMapp + (2 * eps));
+        }
     }
 
 }
